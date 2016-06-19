c62c1297d80f24b3bf646813a61acb9052bafc8a
Nakamura Agatha <nakamuraagatha@mail.com>
2016-06-20 03:05:33 +0700

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 ############################################
 
 set :stage, :production
-set :stage_url, "http://www.example.com"
-server "XXX.XXX.XX.XXX", user: "SSHUSER", roles: %w{web app db}
-set :deploy_to, "/deploy/to/path"
+set :stage_url, "http://139.59.246.121"
+server "139.59.246.121", user: "root", roles: %w{web app db}
+set :deploy_to, "/var/www/html"
 
 ############################################
 # Setup Git
